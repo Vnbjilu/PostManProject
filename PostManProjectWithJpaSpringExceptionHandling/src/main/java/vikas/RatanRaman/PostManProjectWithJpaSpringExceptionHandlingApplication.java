eff2c159ae6bca28ca444b6040edad7a06165e9a
Vnbjilu <vikas1.srivastava@gmail.com>
2022-10-14 08:57:58 +0530

    first Commit with Configuration and Project Structure

---
+++ PostManProjectWithJpaSpringExceptionHandling/src/main/java/vikas/RatanRaman/PostManProjectWithJpaSpringExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package vikas.RatanRaman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostManProjectWithJpaSpringExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostManProjectWithJpaSpringExceptionHandlingApplication.class, args);
+	}
+
+}
